fd57445823fe79ba2cb9f76a01e55cd5ec400aaf
Steve Counsell <steve@stevecounsell.com>
2015-04-02 10:17:50 +0100

    no message

--- home.php
+++ home.php
@@ -35,31 +35,9 @@ function minimum_home_featured() {
 		genesis_widget_area( 'home-featured-2', array(
 			'before' => '<div class="home-featured-2 widget-area">',
 		) );
-
-	echo '</div><!-- end .wrap --></div><!-- end #home-featured XILO -->';
-
+?>
+	</div>
+</div>
+<?php
 }
-
-/*
-add_action( 'genesis_loop', 'minimum_grid_loop_helper' );
-function minimum_grid_loop_helper() {
-
-	if ( function_exists( 'genesis_grid_loop' ) ) {
-		genesis_grid_loop( array(
-			'features' 				=> 0,
-			'feature_image_size' 	=> 'featured',
-			'feature_image_class' 	=> 'post-image',
-			'feature_content_limit' => 0,
-			'grid_image_size'		=> 0,
-			'grid_image_class'		=> 'alignnone',
-			'grid_content_limit' 	=> 250,
-			'more'					=> __( '[Read more]', 'minimum' ),
-		) );
-	} else {
-		genesis_standard_loop();
-	}
-
-}
-*/
-
 genesis();
